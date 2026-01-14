ee987dada3a8b2478bafff78b81e046da9fa4632
Oliver <s234993@student.dtu.dk>
2026-01-14 14:36:29 +0100

    added a stream model

---
+++ lib/model/streamModel.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:movesense_plus/movesense_plus.dart';
+
+import 'sensorModel.dart';
+import 'deviceModel.dart';
+
+//'0C:8C:DC:1B:23:16'
+class StreamModel{
+  final MovesenseDev device;
+  final Sensor lightSensor = LightSensor();
+
+  StreamSubscription<MovesenseHR>? hrSubscription;
+  StreamSubscription<String>? lightSubscription;
+
+  StreamModel(this.device){
+    if(device.status == DeviceConnectionStatus.connected){
+      lightSensor.start();
+      hrSubscription = device.hr.listen((hr) {
+      //Timer.periodic(Duration(minutes: 5), (_) {
+      //saveToDB();
+    //});
+      debugPrint('Heart Rate: ${hr.average}, r-r: ${hr.rr}');
+      });
+    }else{
+      /// Throws an exception when the device is not connected.
+      /// 
+      /// This exception will halt execution and must be caught by a try-catch block
+      /// or it will propagate up the call stack, terminating the current function
+      /// and any subsequent code in that block will not execute.
+      throw Exception('Device not connected');
+    }
+  }
+}
