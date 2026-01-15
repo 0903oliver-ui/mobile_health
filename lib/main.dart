49174c10dca0014fa6919825e5f3c69acb5f0bdd
jakobegestad <s234991@student.dtu.dk>
2026-01-15 15:22:36 +0100

    Fix case-only rename and cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,14 @@ import 'dart:developer';
 part 'view/firstScreen_view.dart';
 part 'view/registerScreen_view.dart';
 part 'view/homePage_view.dart';
+part 'view/VerificationScreen_view.dart';
+part 'view/connectionsScreen_view.dart';
+part 'view/loadingScreen_view.dart';
+
 part 'view_model/firstScreen_viewModel.dart';
 part 'view_model/homepage_viewModel.dart';
 part 'view_model/registerScreen_viewModel.dart';
-part 'view/connectionsScreen_view.dart';
 part 'view_model/connectionsScreen_viewModel.dart';
-part 'view/loadingScreen_view.dart';
 part 'view_model/loadingScrenn_viewModel.dart';
 
 part 'model/deviceModel.dart';
@@ -44,7 +46,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textTheme: GoogleFonts.merriweatherTextTheme(),
       ),
-      home: FirstScreenView(),
+      //home: FirstScreenView(),
+      home: VerificationscreenView()
     );
   }
 }
